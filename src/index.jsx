dd599b49f3b024446b70191a65f6130e5c87f064
Frank Weindel <6070611+frank-weindel@users.noreply.github.com>
2023-09-19 06:15:52 -0400

    Updates for release

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { render, Canvas, Config } from '@lightningjs/solid';
 import { Router } from "@solidjs/router";
 import App from './pages/App';
 import coreExtensionModuleUrl from './AppCoreExtensions.js?importChunkUrl';
+import coreWorkerUrl from './threadx-core-worker.js?importChunkUrl';
 
 Config.debug = false;
 Config.fontSettings.fontFamily = 'Ubuntu';
@@ -11,9 +12,11 @@ Config.keyMap.m = 'Menu';
 Config.keyMap.t = 'Text';
 Config.keyMap.b = 'Buttons';
 
+const driver = 'threadx'; // Use 'main' for main thread-only rendering
+
 const RenderOptions = {
   coreExtensionModule: coreExtensionModuleUrl,
-  driver: 'threadx',
+  threadXCoreWorkerUrl: driver === 'threadx' ? coreWorkerUrl : undefined,
   // deviceLogicalPixelRatio: 1
 }
 
