d7eb5a4ca47adc41493e16ba065baa3185501e76
Chris Lorenzo <christopher_lorenzo@comcast.com>
2023-11-09 12:00:11 -0500

    Updates

---
+++ assets/AppCoreExtensions-46a576c0.js
@@ -0,0 +1,2 @@
+import{C as a,W as e,S as s}from"./CoreExtension-c81fcba3.js";function r(){import.meta.url,import("_").catch(()=>1);async function*o(){}}const t="/";class c extends a{async run(n){n.fontManager.addFontFace(new e("NotoSans",{},t+"fonts/NotoSans-Regular.ttf")),n.fontManager.addFontFace(new s("Ubuntu",{},"msdf",n,t+"fonts/Ubuntu-Bold.msdf.png",t+"fonts/Ubuntu-Bold.msdf.json"))}}export{r as __vite_legacy_guard,c as default};
+//# sourceMappingURL=AppCoreExtensions-46a576c0.js.map
--- assets/AppCoreExtensions-4dc27172.js
+++
@@ -1,2 +0,0 @@
-import{C as a,W as e,S as s}from"./CoreExtension-d763954d.js";function r(){import.meta.url,import("_").catch(()=>1);async function*t(){}}const o="/solid-demo-app/";class f extends a{async run(n){n.fontManager.addFontFace(new e("NotoSans",{},o+"fonts/NotoSans-Regular.ttf")),n.fontManager.addFontFace(new s("Ubuntu",{},"msdf",n,o+"fonts/Ubuntu-Bold.msdf.png",o+"fonts/Ubuntu-Bold.msdf.json"))}}export{r as __vite_legacy_guard,f as default};
-//# sourceMappingURL=AppCoreExtensions-4dc27172.js.map
