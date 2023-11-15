0091360ddfbb3dfa7d5d6326cf95e69d57a69233
Chris Lorenzo <christopher_lorenzo@comcast.com>
2023-11-15 09:30:19 -0500

    lower background image to 720p

--- src/api/formatters/ItemFormatter.ts
+++ src/api/formatters/ItemFormatter.ts
@@ -38,7 +38,7 @@ export function convertItemsToTiles(items: any[] = []): Tile[] {
   return items.map((item) => ({
       src: getImageUrl(item.poster_path || item.profile_path),
       tileSrc: getImageUrl(item.backdrop_path || item.profile_path, 'w300'),
-      backdrop: getImageUrl(item.backdrop_path, 'original'),
+      backdrop: getImageUrl(item.backdrop_path, 'w1280'),
       href: `${import.meta.env.BASE_URL}entity/${item.media_type || 'people'}/${item.id}`,
       shortTitle: truncateString(item.title || item.name, 30),
       title: item.title || item.name,
