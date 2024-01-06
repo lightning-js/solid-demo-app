f4065b878389b733f30779f29572c114f092b36e
Chris Lorenzo <christopher_lorenzo@comcast.com>
2024-01-06 17:18:43 -0500

    Updates

--- assets/AppCoreExtensions-406f6f1c.js
+++
@@ -1,2 +0,0 @@
-import{C as a,W as e,S as s}from"./CoreExtension-fb417e8a.js";function r(){import.meta.url,import("_").catch(()=>1);async function*t(){}}const o="/solid-demo-app/";class f extends a{async run(n){n.fontManager.addFontFace(new e("NotoSans",{},o+"fonts/NotoSans-Regular.ttf")),n.fontManager.addFontFace(new s("Ubuntu",{},"msdf",n,o+"fonts/Ubuntu-Bold.msdf.png",o+"fonts/Ubuntu-Bold.msdf.json"))}}export{r as __vite_legacy_guard,f as default};
-//# sourceMappingURL=AppCoreExtensions-406f6f1c.js.map
